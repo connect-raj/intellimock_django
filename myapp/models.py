82edf8995e9f9c35296b92c8893469066f5a4299
Raj <connect.rajtrivedi@gmail.com>
2025-03-21 00:14:26 +0530

    "fix: uuid length"

--- myapp/models.py
+++ myapp/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    userId = models.CharField(max_length=30, primary_key=True)
+    userId = models.CharField(max_length=40, primary_key=True)
     userFullName = models.CharField(max_length=40, null=False)
     userEmail = models.EmailField(max_length=255, null=False)
     userPassword = models.CharField(max_length=18, null=False)
+    userType = models.CharField(max_length=20, null=False, default='user')
     createdAt = models.DateTimeField(auto_now_add=True)
 
 class Resume(models.Model):
-    resumeId = models.CharField(max_length=30, primary_key=True)
+    resumeId = models.CharField(max_length=40, primary_key=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     cloudUrl = models.URLField(blank=True, null=True)
     skills = models.JSONField()
@@ -17,21 +18,21 @@ class Resume(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class Interview(models.Model):
-    interviewId = models.CharField(max_length=30, primary_key=True)
+    interviewId = models.CharField(max_length=40, primary_key=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
     skill = models.CharField(max_length=20)
     level = models.CharField(max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class mockInterview(models.Model):
-    mockInterviewId = models.CharField(max_length=30, primary_key=True)
+    mockInterviewId = models.CharField(max_length=40, primary_key=True)
     interviewId = models.ForeignKey(Interview, on_delete=models.CASCADE)
     feedBack = models.JSONField()
     timeStamp = models.DateTimeField(auto_now_add=True)
 
 
 class codeInterview(models.Model):
-    codeInterviewId = models.CharField(max_length=30, primary_key=True)
+    codeInterviewId = models.CharField(max_length=40, primary_key=True)
     interviewId = models.ForeignKey(Interview, on_delete=models.CASCADE)
     language = models.CharField(max_length=30)
     time = models.CharField(max_length=50)
@@ -42,16 +43,48 @@ class codeInterview(models.Model):
     timeStamp = models.DateTimeField(auto_now_add=True)
     
 class practiceQuestion(models.Model):
-    questionId = models.CharField(max_length = 30, primary_key=True)
+    questionId = models.CharField(max_length = 40, primary_key=True)
     question= models.CharField(max_length=100)
     Type = models.CharField(max_length=100)
     skill= models.CharField(max_length=100)
     level = models.CharField(max_length=100)
 
 class comments(models.Model):
-    commentId = models.CharField(max_length = 30, primary_key=True)
+    commentId = models.CharField(max_length = 40, primary_key=True)
     questionId = models.ForeignKey(practiceQuestion,max_length= 30, on_delete=models.CASCADE)
     userId = models.ForeignKey(User,max_length= 30, on_delete=models.CASCADE)
     userFullName = models.CharField(max_length=50)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class Company(models.Model):
+    companyId = models.CharField(max_length = 40, primary_key=True)
+    companyName = models.CharField(max_length=100)
+    companyEmail = models.EmailField()
+    companyNumber = models.IntegerField()
+    companyWebsite = models.URLField()
+    companyAbout = models.TextField()
+    companyAddress = models.CharField(max_length=200)
+
+class JobRole(models.Model):
+    jobRoleId = models.CharField(max_length = 40, primary_key=True)
+    companyId = models.ForeignKey(Company, on_delete=models.CASCADE)
+    jobTitle = models.CharField(max_length = 50)
+    jobDescription = models.TextField()
+    jobOpeningAvailable = models.IntegerField()
+    jobConstraints = models.JSONField()
+    jobExpireTime = models.IntegerField()
+
+class Applicants(models.Model):
+    applicantId = models.CharField(max_length = 40, primary_key=True)
+    userId = models.ForeignKey(User, on_delete = models.CASCADE)
+    userName = models.CharField(max_length = 30)
+    userEmail = models.EmailField()
+    skills = models.TextField()
+    experience = models.IntegerField()
+
+
+class IntSchedule(models.Model):
+    scheduleId = models.CharField(max_length = 40, primary_key=True)
+    applicantId = models.ForeignKey(Applicants, on_delete = models.CASCADE)
+    intData = models.JSONField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+import uuid
 
 # Create your views here.
+@csrf_exempt
+def logIn(request):
+    
+    if request.method == 'POST':
+        username = 'admin'
+        password = 'admin'
+
+        if username == 'admin' and password == 'admin':
+            return HttpResponse('Success')
+
+    return HttpResponse('error')
+
+
+def signUp(request):
+
+    if request.method == 'POST':
+        userFullName = request.POST.get('userName')
+        userEmail = request.POST.get('userEmail')
+        userPassword = request.POST.get('userPassword')
+        userType = request.POST.get('userType')
+
+        userInstance = User.objects.create(userId = uuid.uuid4(),userFullName=userFullName, userEmail=userEmail, userPassword=userPassword, userType=userType)
